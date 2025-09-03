a45f3754783951bb20be5709eb029d916fd03335
OhemaaCindy <cindyessuman67@gmail.com>
2025-09-03 21:01:51 +0000

    showed loading state for protected layout

--- src/pages/layouts/auth-layout.tsx
+++ src/pages/layouts/auth-layout.tsx
@@ -1,3 +1,23 @@
+// import { checkAuthUser } from "@/services/auth-services";
+// import type { CheckAuthResponse } from "@/types/auth.type";
+// import { useQuery } from "@tanstack/react-query";
+// import Cookies from "js-cookie";
+// import { Navigate, Outlet } from "react-router";
+
+// const Protectedlayout = () => {
+//   const { data } = useQuery<CheckAuthResponse, Error>({
+//     queryKey: ["get-info"],
+//     queryFn: checkAuthUser,
+//     enabled: !!Cookies.get("token"),
+//   });
+
+//   const user = data?.user;
+
+//   return user ? <Outlet /> : <Navigate to="/" />;
+// };
+
+// export default Protectedlayout;
+
 import { checkAuthUser } from "@/services/auth-services";
 import type { CheckAuthResponse } from "@/types/auth.type";
 import { useQuery } from "@tanstack/react-query";
@@ -5,15 +25,27 @@ import Cookies from "js-cookie";
 import { Navigate, Outlet } from "react-router";
 
 const Protectedlayout = () => {
-  const { data } = useQuery<CheckAuthResponse, Error>({
+  const { data, isLoading, isError } = useQuery<CheckAuthResponse, Error>({
     queryKey: ["get-info"],
     queryFn: checkAuthUser,
     enabled: !!Cookies.get("token"),
   });
 
-  const user = data?.user;
+  if (!Cookies.get("token")) {
+    return <Navigate to="/" />;
+  }
 
-  return user ? <Outlet /> : <Navigate to="/" />;
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  // If error or no user data, redirect
+  if (isError || !data?.user) {
+    return <Navigate to="/" />;
+  }
+
+  // User is authenticated, show protected content
+  return <Outlet />;
 };
 
 export default Protectedlayout;
